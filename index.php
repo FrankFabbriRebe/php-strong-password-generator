67529e9ee07776486447b93005629f615eddc077
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-24 15:28:09 +0100

    add: function to generate random psw

--- index.php
+++ index.php
@@ -11,15 +11,36 @@
 
     <!-- form -->
     <form>
-        <input type="number" name="number">
-        <input type="submit" value="Send">
+        <label for="number">Enter the length of the password:</label>
+        <input type="number" name="number" id="number" required>
+        <input type="submit" value="Generate Password">
     </form>
 
     <?php
 
     $userNumber = $_GET["number"];
-    // echo "il numero scelto dall'utente è $userNumber";
+    // echo "User number is: $userNumber";
     
+    // function to generate random psw
+    function generatePsw($userNumber)
+    {
+
+        // list of characters available
+        $charactersAvailable = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_';
+
+        $randomPsw = '';
+
+        for ($i = 0; $i < $userNumber; $i++) {
+            $randomPsw .= $charactersAvailable[rand(0, strlen($charactersAvailable) - 1)];
+        }
+
+        return $randomPsw;
+
+    }
+
+    echo generatePsw($userNumber);
+
+
     ?>
 
 </body>
