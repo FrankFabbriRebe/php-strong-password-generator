000da8be781c985339c1633eb12076c31aa33fa9
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-24 15:52:10 +0100

    add: function to generate random psw

--- partials/functions.php
+++ partials/functions.php
@@ -1,5 +1,25 @@
 <?php
 
+$userNumber = $_GET["number"];
+// echo "User number is: $userNumber";
+
+// function to generate random psw
+function generatePsw($userNumber)
+{
+
+    // list of characters available
+    $charactersAvailable = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_';
+
+    $randomPsw = '';
+
+    for ($i = 0; $i < $userNumber; $i++) {
+        $randomPsw .= $charactersAvailable[rand(0, strlen($charactersAvailable) - 1)];
+    }
+
+    return $randomPsw;
+
+}
+
 
 
 ?>
